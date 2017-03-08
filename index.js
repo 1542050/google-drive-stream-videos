cb12ef50aafc9cfbaefa10adbb0e0f4627e2a4f3
Kim Bui <1542050@student.hcmus.edu.vn>
2017-03-08 13:40:03 +0700

    create file proxy.js

--- index.js
+++ index.js
@@ -2,7 +2,19 @@
 * @Author: kimbui
 * @Date:   2017-03-08 09:30:11
 * @Last Modified by:   kimbui
-* @Last Modified time: 2017-03-08 09:30:11
+* @Last Modified time: 2017-03-08 13:38:52
 */
 
 'use strict';
+require('dotenv').load();
+var http = require('http')
+const app = require('router')
+
+module.main = http.createServer((req, res) => {
+  console.log(process.env.VIRTUAL_HOST)
+  app(req, res)
+  res.end();
+})
+
+module.main.listen(8000)
+console.log("Server running at http://127.0.0.1:8000/");
