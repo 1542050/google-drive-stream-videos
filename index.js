bf70cad911faab3cf75488ac04ca53719e588808
Kim Bui <1542050@student.hcmus.edu.vn>
2017-03-09 11:36:48 +0700

    update video playback

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 * @Author: kimbui
 * @Date:   2017-03-08 09:30:11
 * @Last Modified by:   kimbui
-* @Last Modified time: 2017-03-09 11:24:33
+* @Last Modified time: 2017-03-09 11:30:47
 */
 
 'use strict';
@@ -10,6 +10,10 @@ require('dotenv').load();
 const getVideo = require('./lib/get-video')
 var http = require('http')
 const app = require('router')()
+app.use(require('./lib/parse-query'))
+
+// Create API Stream Video
+app.get('/videoplayback', require('./lib/videoplayback'))
 
 // Create API Get Video From Google Drive
 app.get('/:provider/:id', function (req, res) {
